e7dc15fae06e0339305230fd3bf24b615742d551
jcpartri <jcpartri@gmail.com>
2026-06-15 16:45:24 -0400

    update

--- unit_004B/python_files/examples.py
+++ unit_004B/python_files/examples.py
@@ -1,7 +1,5 @@
-"""
-Explain when to use a for loop versus a while loop.
-"""
-
-# Use a For loop when you know in advance how many times you want to execute a block of code. For example, if you want to iterate over a list of items or a range of numbers, a for loop is more appropriate.
-
-# Use a While loop when you want to execute a block of code as long as a certain condition is true. This is useful when you don't know in advance how many times you need to execute the code, such as when waiting for user input or when working with dynamic data.
+# 1. Create and understand nested for and while loops
+for outer in range(3):  # Outer loop
+    for inner in range(4):  # Inner loop
+        print(f"({outer}, {inner})", end=" ")
+    print()  # New line after inner loop
