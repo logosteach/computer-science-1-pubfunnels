24feda62a08b2c8166e91c99f6c83520b192c410
jcpartri <jcpartri@gmail.com>
2026-06-29 14:36:11 -0400

    updatre

---
+++ unit_004A/practice/boolean_starter.py
@@ -0,0 +1,20 @@
+# Create a boolean variable
+is_raining = True
+
+if is_raining:
+    print("It's raining! Don't forget your umbrella.")
+else:
+    print("It's not raining. Enjoy your day!")
+
+############################################################
+
+is_raining = False
+
+if is_raining:
+    print("It is still raining!)")
+else:
+    print("The rain has stopped. You can go outside now.")
+
+
+# Copyright (c) 2026 LogosTeach - All Rights Reserved.
+# This practice file is for educational purposes only and may not be used for commercial purposes without permission from LogosTeach.
